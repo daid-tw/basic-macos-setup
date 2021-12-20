5fbe7662477581bfe7552008eb2e34bdc4c57d43
Daidson Alves <daidson.alves@thoughtworks.com>
2021-12-20 09:51:04 -0300

    creating new logic to install apps in order to have vscode updated

--- install_apps.sh
+++ install_apps.sh
@@ -1,17 +1,37 @@
-#Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#!/bin/bash
 
-#Reinitialize terminal
+replace=""
+file="install_apps.sh"
+cd ${0/$file/$replace}
+
+echo "----------"
+#Install proxy on terminal. (bash or zsh)
+sh ./install_proxy.sh
+clear
+
+#Reopen terminal on same instance.
 source ~/.zshrc
+source ~/.bashrc
+source ~/.bash_profile
+
+#Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 #Install bundle
 brew tap Homebrew/bundle
-
-#Reinitialize terminal
-source ~/.zshrc
-
 #Install all apps in Brewfile
 brew bundle
+
+#Installing code command on path
+cat << EOF >> ~/.zprofile
+# Add Visual Studio Code (code)
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+EOF
+
+#Refresh terminal
+source ~/.zshrc
+source ~/.bashrc
+source ~/.bash_profile
 
 #Install vscode extensions
 code --install-extension dbaeumer.vscode-eslint
